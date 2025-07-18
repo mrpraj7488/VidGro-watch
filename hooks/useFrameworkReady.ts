3cbf7fd37b6c29ee8554289f9fd3f62224102191
mrpraj7488 <mr.praj7488@gmail.com>
2025-07-18 23:54:52 +0530

    Add structuredClone polyfill

--- hooks/useFrameworkReady.ts
+++ hooks/useFrameworkReady.ts
@@ -1,3 +1,10 @@
+// Polyfill for structuredClone if not available
+if (typeof globalThis.structuredClone === 'undefined') {
+  globalThis.structuredClone = function(obj: any) {
+    return JSON.parse(JSON.stringify(obj));
+  };
+}
+
 import { useEffect } from 'react';
 
 declare global {
